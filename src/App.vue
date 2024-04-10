35c55c67f259ca3f0f2e5958691376cdb0cb9d74
MatteoGiunta97 <matteogiunta@outlook.it>
2024-04-10 21:03:00 +0200

    feat: add milestone 1

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,23 @@
-/*
+<!-- 
 Descrizione:
 Create un nuovo progetto utilizzando Vite: aiutatevi con le slide per ripercorrere i vari passaggi dell'installazione come visti a lezione.
 Create e utilizzate un componente AppTitle, il quale contiene un titolo che recita "La mia prima app con Vite!"
 Bonus:
-Creare un secondo componente AppHero, che visualizza un jumbotron.
-*/
+Creare un secondo componente AppHero, che visualizza un jumbotron. 
+-->
 
 <script>
+  import AppTitle from './components/AppTitle.vue';
+
   export default {
     components: {
-
+      AppTitle
     }
-
   }
 </script>
 
-
 <template>
-  <h1>Hello</h1>
+
+  <AppTitle></AppTitle>
+
 </template>
